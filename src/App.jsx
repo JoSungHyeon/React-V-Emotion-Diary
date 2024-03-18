603f4fd43d686434a98db0d99ffe906b9b385470
JoSungHyeon <jsh950618@gmail.com>
2024-03-18 16:13:30 +0900

    context commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,124 @@
 import './App.css';
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { useReducer, useRef, createContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
+import Edit from './pages/Edit';
 import Notfound from './pages/Notfound';
 
-import { getEmotionImage } from './util/get-emotion-images';
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+// 3. "/edit" : 일기를 수정하는 Edit 페이지
+// Routes 태그 하위에는 Route 만 렌더링 가능
+// Routes 바깥의 태그는 렌더링 가능
+
+const mockDate = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  }
+];
+
+function reducer(state, action) {
+  switch(action.type){
+    case 'CREATE':
+      return [action.date, ...state];
+    case 'UPDATE':
+      return state.map((item)=>String(item.id) === String(action.data.id) ? action.data : item);
+    case 'DELETE':
+      return state.filter((item)=>String(item.id) !== String(action.id));
+    default:
+      return state;
+  }
+}
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
 
 function App() {
-  // Routes 태그 하위에는 Route 만 렌더링 가능
-  // Routes 바깥의 태그는 렌더링 가능
-  const nav = useNavigate();
+  const [data, dispatch] = useReducer(reducer, mockDate);
+  const idRef = useRef(3);
 
-  const onClickButton = () => {
-    nav("/new");
+  // 새로운 일기 추가
+  const onCreate = (createDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      date : {
+        id: idRef.current++,
+        createDate,
+        emotionId,
+        content,
+      },
+    });
   };
+
+  // 기존 일기 수정
+  const onUpdate = (id, createDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+  // 기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    });
+  };
+  
+  
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<New />} />
-        <Route path='/diary/:id' element={<Diary />} />
-        <Route path='*' element={<Notfound />} />
-      </Routes>
+      <button onClick={()=>{
+        onCreate(new Date().getTime(), 1, "Hello");
+      }}>일기추가 테스트</button>
+
+
+      <button onClick={()=>{
+        onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다.");
+      }}>
+        일기수정 테스트
+      </button>
+
+      <button onClick={()=>{
+        onDelete(1);
+      }}>
+        일기삭제 테스트
+      </button>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider
+          value={{
+            onCreate,
+            onUpdate,
+            onDelete
+          }}
+        >
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/new' element={<New />} />
+          <Route path='/diary/:id' element={<Diary />} />
+          <Route path='/edit/:id' element={<Edit />} />
+          <Route path='*' element={<Notfound />} />
+        </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   )
 }
--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,11 +1,11 @@
 import { useParams } from "react-router-dom";
 
-const Diary = () => {
+const Edit = () => {
     const params = useParams();
-
+    
     return (
         <div>{params.id}번 일기입니다.</div>
     )
 };
 
-export default Diary;
+export default Edit;

c72f021fe3c4ffe63ff35245fbb2d5e7502dd84b
JoSungHyeon <jsh950618@gmail.com>
2024-03-20 10:46:50 +0900

    diary complete

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -2,9 +2,7 @@ import './Viewer.css'
 import { getEmotionImage } from '../util/get-emotion-images'
 import { emotionList } from '../util/constants'
 
-const Viewer = () => {
-    const emotionId = 1;
-
+const Viewer = ({ emotionId, content }) => {
     const emotionItem = emotionList.find((item) => String(item.emotionId) === String(emotionId))
 
     return (
@@ -19,7 +17,7 @@ const Viewer = () => {
             <section className='content_section'>
                 <h4>오늘의 일기</h4>
                 <div className='content_wrapper'>
-                    <p>일기 ...</p>
+                    <p>{content}</p>
                 </div>
             </section>
         </div>
--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Button from "../components/Button";
 import Viewer from "../components/Viewer";
 import useDiary from "../hooks/useDiary";
+import { getStringedDate } from "../util/get-stringed-date";
 
 const Diary = () => {
     const params = useParams();
@@ -10,15 +11,20 @@ const Diary = () => {
 
     const curDiaryItem = useDiary(params.id);
 
-    console.log(curDiaryItem)
+    if(!curDiaryItem) {
+        return <div>데이터 로딩중...</div>
+    }
+
+    const { createdDate, emotionId, content } = curDiaryItem;
+    const title = getStringedDate(new Date(createdDate));
 
     return (
         <div>
-            <Header title={"yyyy-mm-dd 기록"} 
+            <Header title={`${title} 기록`} 
             leftChild={<Button onClick={()=>nav(-1)} text={"< 뒤로 가기"} />}
             rightChild={<Button onClick={()=>nav(`/edit/${params.id}`)} text={"수정하기"} />}
             />
-            <Viewer />
+            <Viewer emotionId={emotionId} content={content} />
         </div>
     )
 };
